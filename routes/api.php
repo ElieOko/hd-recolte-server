c14d267554e28caaa19b58bcee5af195c4423d5a
ElieOko <elieoko100@gmail.com>
2025-04-12 13:14:55 +0100

    feat:route api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AgentController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ClientTrashController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('client/auth', [ClientController::class, 'auth_client']);
+Route::post('agent/auth', [AgentController::class, 'auth_agent']);
+Route::post('trash/clean/{id}', [ClientTrashController::class, 'clean']);
+Route::resource('agent', AgentController::class)->only([ 'index','store']);
+Route::resource('client', ClientController::class)->only(['store','index']);
+Route::resource('trash', ClientTrashController::class)->only(['index', 'store']);
+// Route::resource('state_trash', ClientController::class)->only(['index', 'store']);
+
+Route::middleware(['auth:sanctum'])->group(function () { 
+    // Route::resource('client', ClientController::class)->only(['index', 'store']);
+});
