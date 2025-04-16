cd1c3af9398db292ecd7a24b1c2620e17aa7bdf7
ElieOko <elieoko100@gmail.com>
2025-04-16 11:32:24 +0100

    fix:user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     //
+    // public function login(Request $request) {
+    //     $validator = Validator::make($request->all(),[
+    //         "username" =>'required|string',
+    //         "password" =>'required|string'
+    //     ]);
+    //     if($validator->stopOnFirstFailure()->fails()){
+    //         return response()->json([
+    //             'message' => $validator->errors()
+    //          ],402);
+    //     }
+    //     $field = $validator->validated();
+    //     $user = User::where('username',$field['username'])->first() ;
+    //     if(!$user){
+    //         return response()->json([
+    //            'message' => 'Utilisateur non valide'
+    //         ],404);
+    //     }
+    //     if($field['password'] != $user->password){
+    //         return response()->json([
+    //            'message' => 'Mot de passe incorrecte'
+    //         ],404);
+    //     }
+       
+    // }
+
+    // public function register(Request $request) {
+    //     $data["sys"] ="User System";
+    //     $account = "User Sytem";
+    //     $validator = Validator::make($request->all(),[
+    //         'password' => 'string',
+    //         'email'=>'string',
+    //     ]);
+
+    //     if($validator->stopOnFirstFailure()->fails()){
+    //         return response()->json([
+    //             'message' => $validator->errors(),
+    //          ],403);
+    //     }
+    //     // $field = $validator->validated();
+       
+    //     //     $token = $user->createToken('token')->plainTextToken;
+    // }
 }
